7d0444841f054fe1220227c7148d24acffb9e7b0
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2019-11-28 23:35:05 +0200

    add the two players' playground condition.
    whenever the ball hits a player's playground the program halts

--- project/project.asm
+++ project/project.asm
@@ -1,15 +1,16 @@
 .MODEL SMALL
 .STACK 64
 .DATA
-	WINDOW_WIDTH DW 140h   ;the width of the window (320 pixels)
-	WINDOW_HEIGHT DW 0C8h  ;the height of the window (200 pixels)
+	WINDOW_WIDTH EQU 320   ;the width of the window (320 pixels)
+	WINDOW_HEIGHT EQU 200  ;the height of the window (200 pixels)
+	WINDOW_HALF_HEIGHT EQU 100  
 	WINDOW_BOUNDS EQU 6    ;variable used to check collisions early
 						   ;must be more than the ball size to avoid ball glitching
 						   
 	TIME_AUX DB 0 ;variable used when checking if the time has changed
 	
-	BALL_X DW 20 ;X position (column) of the ball
-	BALL_Y DW 20 ;Y position (line) of the ball
+	BALL_X DW 180 ;X start position (column) of the ball 
+	BALL_Y DW 100 ;Y start position (line) of the ball
 	BALL_SIZE EQU 08h ;size of the ball (how many pixels does the ball have in width and height)
 	BALL_VELOCITY_X DW 04H ;X (horizontal) velocity of the ball
 	BALL_VELOCITY_Y DW 02H ;Y (vertical) velocity of the ball
@@ -17,18 +18,25 @@
 	;;;;;;;;;;;;;;;;;
 	WALL_X EQU 155
 	WALL_Y EQU 100
-	WALL_SIZE_X EQU 10
-	WALL_SIZE_Y EQU 100
+	WALL_WIDTH EQU 10
+	WALL_HIGHT EQU 100
 	;;;;;;;;;;;;;;;;;
 	
 	;;;;;;;;;;;;;;;;;           Player One           ;;;;;;;;;;;;;;;;;         
 	PLAYER_ONE_X DW 0H   			;X position of the first player
 	PLAYER_ONE_Y DW 0A0H			;Y position of the first player
-	PLAYER_ONE_X_SIZE DW 0AH 		;size of the first player in X direction
-	PLAYER_ONE_Y_SIZE DW 14H 		;size of the first player in Y direction
+	PLAYER_ONE_WIDTH DW 0AH 		;size of the first player in X direction
+	PLAYER_ONE_HIGHT DW 14H 		;size of the first player in Y direction
 	PLAYER_VELOCITY_X DW 0AH      ;X (horizontal) velocity of the player
 	PLAYER_VELOCITY_Y DW 0FH      ;Y (vertical) velocity of the player
 	;;;;;;;;;;;;;;;;;
+	
+	;player one playground
+	PLAYER_ONE_PLAYGROUND_X_START EQU WINDOW_BOUNDS
+	PLAYER_ONE_PLAYGROUND_X_END EQU (WALL_X - WINDOW_BOUNDS)
+	;player two playground
+	PLAYER_TWO_PLAYGROUND_X_START EQU (WALL_X + WALL_WIDTH + WINDOW_BOUNDS)
+	PLAYER_TWO_PLAYGROUND_X_END EQU (WINDOW_WIDTH - WINDOW_BOUNDS)
 	
 .CODE 
 	MAIN PROC FAR
@@ -79,6 +87,15 @@
 		;JE NEG_VELOCITY_Y
 		;;;;;;;;;;;;;;
 		
+		;check player one playground
+		CALL CHECK_PLAYER_ONE_PLAYGROUND
+		CMP AX, 1
+		JE HALT1
+		
+		CALL CHECK_PLAYER_TWO_PLAYGROUND
+		CMP AX, 1
+		JE HALT1
+		
 		MOV AX,WINDOW_BOUNDS
 		CMP BALL_X,AX                         
 		JL NEG_VELOCITY_X         ;BALL_X < 0 + WINDOW_BOUNDS 
@@ -96,12 +113,6 @@
 		CMP BALL_Y,AX   ;BALL_Y < 0 + WINDOW_BOUNDS (Y -> collided)
 		JL NEG_VELOCITY_Y                          
 		
-		MOV AX,WINDOW_HEIGHT	
-		SUB AX,BALL_SIZE
-		SUB AX,WINDOW_BOUNDS
-		CMP BALL_Y,AX
-		JG NEG_VELOCITY_Y		  ;BALL_Y > WINDOW_HEIGHT - BALL_SIZE - WINDOW_BOUNDS (Y -> collided)
-		
 		RET
 		
 		NEG_VELOCITY_X:
@@ -111,6 +122,9 @@
 		NEG_VELOCITY_Y:
 			NEG BALL_VELOCITY_Y   ;BALL_VELOCITY_Y = - BALL_VELOCITY_Y
 			RET
+		
+		HALT1:
+		HLT
 		
 	MOVE_BALL ENDP
 	
@@ -169,7 +183,7 @@
 			INC CX     ;CX = CX + 1
 			MOV AX,CX          ;CX - WALL_X > WALL_SIZE (Y -> We go to the next line,N -> We continue to the next column
 			SUB AX,WALL_X
-			CMP AX,WALL_SIZE_X
+			CMP AX,WALL_WIDTH
 			JNG DRAW_WALL_HORIZONTAL
 			
 			MOV CX,WALL_X ;the CX register goes back to the initial column
@@ -177,7 +191,7 @@
 			
 			MOV AX,DX              ;DX - WALL_Y > WALL_SIZE (Y -> we exit this procedure,N -> we continue to the next line
 			SUB AX,WALL_Y
-			CMP AX,WALL_SIZE_Y
+			CMP AX,WALL_HIGHT
 			JNG DRAW_WALL_HORIZONTAL
 		
 		RET
@@ -197,7 +211,7 @@
 			INC CX 					;CX = CX + 1
 			MOV AX, CX				;TMP FOR THE SUBTRACTION
 			SUB AX, PLAYER_ONE_X  	;AX = AX - PLAYER_ONE_X
-			CMP AX, PLAYER_ONE_X_SIZE 	;If AX - PLAYER_ONE_X > PLAYER_ONE_X_SIZE
+			CMP AX, PLAYER_ONE_WIDTH 	;If AX - PLAYER_ONE_X > PLAYER_ONE_WIDTH
 			JNG Draw 				;Go to the next line if not greater just proceed drawing horizontally
 			
 			MOV CX, PLAYER_ONE_X		;Start drawing from the begining in the second line
@@ -205,7 +219,7 @@
 				
 			MOV AX, DX				;TMP FOR THE SUBTRACTION
 			SUB AX, PLAYER_ONE_Y  	;AX = AX - PLAYER_ONE_Y
-			CMP AX, PLAYER_ONE_Y_SIZE 	;If AX - PLAYER_ONE_Y > PLAYER_ONE_Y_SIZE
+			CMP AX, PLAYER_ONE_HIGHT 	;If AX - PLAYER_ONE_Y > PLAYER_ONE_HIGHT
 			JNG Draw 				;exit the proc if not greater just proceed drawing
 			
 		RET
@@ -279,7 +293,7 @@
 	MOV AX, 0
 	CMP BALL_X, (WALL_X - WINDOW_BOUNDS)
 	JB RETURN
-	CMP BALL_X, (WALL_X + WALL_SIZE_X + WINDOW_BOUNDS) 
+	CMP BALL_X, (WALL_X + WALL_WIDTH + WINDOW_BOUNDS) 
 	JA RETURN
 	CMP BALL_Y, WALL_Y
 	JB RETURN
@@ -292,7 +306,7 @@
 	MOV AX, 0
 	CMP BALL_X, WALL_X
 	JB RETURN2
-	CMP BALL_X, (WALL_X + WALL_SIZE_X)
+	CMP BALL_X, (WALL_X + WALL_WIDTH)
 	JA RETURN2
 	CMP BALL_Y, WALL_Y
 	JB RETURN2
@@ -303,4 +317,31 @@
 	RET 
 	CHECK_WALL_Y ENDP
 	
-end MAIN 
+	CHECK_PLAYER_ONE_PLAYGROUND PROC NEAR
+	MOV AX, 0
+	CMP BALL_X, PLAYER_ONE_PLAYGROUND_X_START
+	JB RET_CHECK_PLAYER_ONE_PLAYGROUND
+	CMP BALL_X, PLAYER_ONE_PLAYGROUND_X_END 
+	JA RET_CHECK_PLAYER_ONE_PLAYGROUND
+	CMP BALL_Y, (WINDOW_HEIGHT - WINDOW_BOUNDS - BALL_SIZE)	
+	JB RET_CHECK_PLAYER_ONE_PLAYGROUND
+	MOV AX, 1
+	RET_CHECK_PLAYER_ONE_PLAYGROUND:
+	RET 
+	CHECK_PLAYER_ONE_PLAYGROUND ENDP
+	
+	CHECK_PLAYER_TWO_PLAYGROUND PROC NEAR
+	MOV AX, 0
+	CMP BALL_X, PLAYER_TWO_PLAYGROUND_X_START
+	JB RET_CHECK_PLAYER_TWO_PLAYGROUND
+	CMP BALL_X, PLAYER_TWO_PLAYGROUND_X_END 
+	JA RET_CHECK_PLAYER_TWO_PLAYGROUND
+	CMP BALL_Y, (WINDOW_HEIGHT - WINDOW_BOUNDS - BALL_SIZE)	
+	JB RET_CHECK_PLAYER_TWO_PLAYGROUND
+	MOV AX, 1
+	RET_CHECK_PLAYER_TWO_PLAYGROUND:
+	RET 
+	CHECK_PLAYER_TWO_PLAYGROUND ENDP
+	
+	
+END MAIN 
