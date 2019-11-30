501da1e40a119915e4a78a137a0eb8b54b4b7fcb
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2019-11-30 18:10:49 +0200

    Delete macros because of lots of edits. We will add it later

--- project/project.asm
+++ project/project.asm
@@ -52,7 +52,7 @@ include utility.inc
 	MOV AX, @DATA
 	MOV DS, AX
 	
-		ClS ;CALL CLEAR_SCREEN
+		CALL ClS ;CALL CLEAR_SCREEN
 			
 		CHECK_TIME:
 		
@@ -65,12 +65,12 @@ include utility.inc
 			
 			MOV TIME_AUX,DL ;update time
 			
-			CLS ;CALL CLEAR_SCREEN
+			CALL CLS ;CALL CLEAR_SCREEN
 			
 			CALL MOVE_PLAYER_ONE		  ;get move from the user using the keyboard
 			CALL MOVE_PLAYER_TWO
 			
-			MOVE_BALL
+			CALL MOVE_BALL
 			
 			CALL DRAW_WALL
 			MOV AX, PLAYER_ONE_X
@@ -80,7 +80,7 @@ include utility.inc
 			MOV BX, PLAYER_TWO_Y
 			CALL DRAW_PLAYER    ;NOTE: this proc affects the flag registers, BE CAREFULL 
 			
-			DRAW_BALL; CALL DRAW_BALL 
+			CALL DRAW_BALL; CALL DRAW_BALL 
 			
 			JMP CHECK_TIME ;after everything checks time again
 			
@@ -88,6 +88,158 @@ include utility.inc
 		MOV AH, 4CH
 		INT 21H
 	MAIN ENDP
+	
+	CLS PROC NEAR
+		MOV AH,00h ;set the configuration to video mode
+		MOV AL,13h ;choose the video mode
+		INT 10h    ;execute the configuration 
+
+		MOV AH,0Bh ;set the configuration
+		MOV BH,00h ;to the background color
+		MOV BL,00h ;choose black as background color
+		INT 10h    ;execute the configuration
+	CLS ENDP
+	
+	MOVE_BALL PROC NEAR
+		
+		MOV AX,BALL_VELOCITY_X    
+		ADD BALL_X,AX             ;move the ball horizontally
+		MOV AX,BALL_VELOCITY_Y
+		ADD BALL_Y,AX             ;move the ball vertically
+		
+		;check x
+		
+		;check window
+		MOV AX, BALL_SIZE
+		CMP BALL_X,AX                         
+		JL NEG_VELOCITY_X         ;BALL_X < 0   
+		
+		MOV AX,WINDOW_WIDTH
+		SUB AX,BALL_SIZE
+		CMP BALL_X,AX	          ;BALL_X > WINDOW_WIDTH - BALL_SIZE  -  (Y -> collided)
+		JG NEG_VELOCITY_X
+		
+		;check wall
+		CALL CHECK_WALL_X
+		CMP AX, 1
+		JE NEG_VELOCITY_X
+		
+		;CHECK_PLAYER_ONE_X
+		MOV AX, PLAYER_ONE_X
+		MOV BX, PLAYER_ONE_Y
+		CALL CHECK_PLAYER_X
+		CMP AX, 1
+		JE NEG_VELOCITY_X_PLAYER
+		
+		;CHECK_PLAYER_TWO_X
+		MOV AX, PLAYER_TWO_X
+		MOV BX, PLAYER_TWO_Y
+		CALL CHECK_PLAYER_X
+		CMP AX, 1
+		JE NEG_VELOCITY_X_PLAYER
+		
+		JMP CHECK_Y
+			
+		NEG_VELOCITY_X:
+			NEG BALL_VELOCITY_X   ;BALL_VELOCITY_X = - BALL_VELOCITY_X
+			
+		NEG_VELOCITY_X_PLAYER:
+			NEG BALL_VELOCITY_X   ;BALL_VELOCITY_X = - BALL_VELOCITY_X
+			MOV AX,BALL_VELOCITY_X    
+			ADD AX, 1
+			ADD BALL_X,AX             ;move the ball horizontally
+		
+		CHECK_Y:
+		
+		;check window
+		MOV AX, BALL_SIZE
+		CMP BALL_Y,AX   ;BALL_Y < 0 +  (Y -> collided)
+		JL NEG_VELOCITY_Y                          
+		
+		;check wall y
+		CALL CHECK_WALL_Y
+		CMP AX, 1
+		JE NEG_VELOCITY_Y
+		
+		;check player one playground
+		CALL CHECK_PLAYER_ONE_PLAYGROUND
+		CMP AX, 1
+		JE NEG_VELOCITY_Y
+		
+		;check player two playground
+		CALL CHECK_PLAYER_TWO_PLAYGROUND
+		CMP AX, 1
+		JE NEG_VELOCITY_Y
+		
+		;CHECK_PLAYER_ONE_TOP_Y
+		MOV AX, PLAYER_ONE_X
+		MOV BX, PLAYER_ONE_Y
+		CALL CHECK_PLAYER_TOP_Y
+		CMP AX, 1
+		JE NEG_VELOCITY_Y_PLAYER
+		
+		;CHECK_PLAYER_TWO_TOP_Y
+		MOV AX, PLAYER_TWO_X
+		MOV BX, PLAYER_TWO_Y
+		CALL CHECK_PLAYER_TOP_Y
+		CMP AX, 1
+		JE NEG_VELOCITY_Y_PLAYER
+		
+		;CHECK_PLAYER_ONE_DOWN_Y
+		MOV AX, PLAYER_ONE_X
+		MOV BX, PLAYER_ONE_Y
+		CALL CHECK_PLAYER_DOWN_Y
+		CMP AX, 1
+		JE NEG_VELOCITY_Y_PLAYER
+		
+		;CHECK_PLAYER_TWO_DOWN_Y
+		MOV AX, PLAYER_TWO_X
+		MOV BX, PLAYER_TWO_Y
+		CALL CHECK_PLAYER_DOWN_Y
+		CMP AX, 1
+		JE NEG_VELOCITY_Y_PLAYER
+		
+		JMP RET_MOVE_BALL
+				
+		NEG_VELOCITY_Y:
+			NEG BALL_VELOCITY_Y   ;BALL_VELOCITY_Y = - BALL_VELOCITY_Y
+			
+		NEG_VELOCITY_Y_PLAYER:
+			NEG BALL_VELOCITY_Y   ;BALL_VELOCITY_Y = - BALL_VELOCITY_Y
+			MOV AX, BALL_VELOCITY_Y
+			ADD AX, 1
+			ADD BALL_Y,AX             ;move the ball vertically
+		
+		RET_MOVE_BALL:
+			RET
+	MOVE_BALL ENDP
+	
+	DRAW_BALL PROC NEAR
+		
+		MOV CX,BALL_X ;set the initial column (X)
+		MOV DX,BALL_Y ;set the initial line (Y)
+		
+		DRAW_BALL_HORIZONTAL:
+			MOV AH,0Ch ;set the configuration to writing a pixel
+			MOV AL,0Fh ;choose white as color
+			MOV BH,00h ;set the page number 
+			INT 10h    ;execute the configuration
+			
+			INC CX     ;CX = CX + 1
+			MOV AX,CX          ;CX - BALL_X > BALL_SIZE (Y -> We go to the next line,N -> We continue to the next column
+			SUB AX,BALL_X
+			CMP AX,BALL_SIZE
+			JNG DRAW_BALL_HORIZONTAL
+			
+			MOV CX,BALL_X ;the CX register goes back to the initial column
+			INC DX        ;we advance one line
+			
+			MOV AX,DX              ;DX - BALL_Y > BALL_SIZE (Y -> we exit this procedure,N -> we continue to the next line
+			SUB AX,BALL_Y
+			CMP AX,BALL_SIZE
+			JNG DRAW_BALL_HORIZONTAL
+		RET
+	DRAW_BALL ENDP
 	
 	DRAW_WALL PROC NEAR
 		
